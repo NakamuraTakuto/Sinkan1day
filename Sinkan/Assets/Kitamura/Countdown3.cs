ae0f6544edfd8b174886b5045e9fdaa4df39d50a
Kitamura Haruki <90311063+629Kitamura@users.noreply.github.com>
2023-05-25 14:52:53 +0900

    スタイリッシュゲージのサウンド

--- Sinkan/Assets/Kitamura/Countdown3.cs
+++ Sinkan/Assets/Kitamura/Countdown3.cs
@@ -9,13 +9,21 @@ public class Countdown3 : MonoBehaviour
     public float CountDownTime = 3;
     public int StylishStage = 0;
     public int StylishStageMax = 6;
+    public int Score = 0;
     string[] _list = new string[] { "", "E", "D", "C", "B", "A", "S" };
+    int[] _scorePoint = new int[] { 0, 100, 200, 300, 400, 500, 600 };
+    float[] _pitch = new float[] { 0.5f, 0.6f, 0.8f, 1, 1.2f, 1.4f, 1.6f };
     [SerializeField] List<Color> _listColor = new List<Color>();
-    [SerializeField] Slider _stylishSlider;
+    [SerializeField] GameObject _slider;
+    Slider _stylishSlider;
     [SerializeField] Text _stylishText;
+    [SerializeField] Text _scoreText;
+    [SerializeField] AudioSource _stylishAudioSource;
+    [SerializeField] AudioClip _stylishAudioClip;
     // Start is called before the first frame update
     void Start()
     {
+        _stylishSlider = _slider.GetComponent<Slider>();
         _stylishSlider.maxValue = CountDownTimeMax;
     }
 
@@ -36,8 +44,22 @@ public class Countdown3 : MonoBehaviour
             StylishgaugePlus();
         }
 
+        UIs();
+    }
+
+    private void UIs()
+    {
         _stylishText.text = _list[StylishStage];
         _stylishText.color = _listColor[StylishStage];
+        _scoreText.text = Score.ToString();
+        if (StylishStage == 0)
+        {
+            _slider.SetActive(false);
+        }
+        else
+        {
+            _slider.SetActive(true);
+        }
     }
 
     public void StylishgaugePlus()
@@ -45,9 +67,12 @@ public class Countdown3 : MonoBehaviour
         if (StylishStage < StylishStageMax)
         {
             StylishStage += 1;
-            CountDownTime = CountDownTimeMax;
         }
+        _stylishAudioSource.pitch = _pitch[StylishStage];
+        _stylishAudioSource.PlayOneShot(_stylishAudioClip);
 
+        CountDownTime = CountDownTimeMax;
 
+        Score += _scorePoint[StylishStage];
     }
 }
